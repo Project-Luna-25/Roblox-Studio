2cb08e6f77b1d4aebb757d1d9cfcb7b878de5e73
coder-penguin-2012 <201780551+coder-penguin-2012@users.noreply.github.com>
2025-04-18 12:03:38 +0100

    Update disappearingblock.lua
    code added

--- disappearingblock.lua
+++ disappearingblock.lua
@@ -1,1 +1,6 @@
-
+local part = script.Parent
+local plr = game.Players.LocalPlayer
+part.Touched:Connect(function(hit)
+      if hit.Parent.Name == plr.Name then
+          part:Destroy()
+      end
